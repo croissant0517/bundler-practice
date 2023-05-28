a0a46314853b2d2bd66359faad1ae52836ef2c4a
croissant <wathyic@gmail.com>
2023-05-28 20:36:23 +0800

    add loading spinner

--- parcel/src/App.js
+++ parcel/src/App.js
@@ -4,15 +4,20 @@ import "./App.scss";
 
 function App() {
   const [state, setState] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
     console.log("Hello world!");
   };
 
   useEffect(() => {
+    setLoading(true);
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
-      .then((data) => setState(data));
+      .then((data) => {
+        setState(data);
+        setLoading(false);
+      });
   }, []);
 
   return (
@@ -22,9 +27,13 @@ function App() {
       <Button variant="contained" onClick={handleClick}>
         Hello World
       </Button>
-      {state.map((item, index) => {
-        return <div key={index}>{item.title}</div>;
-      })}
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        state.map((item, index) => {
+          return <div key={index}>{item.title}</div>;
+        })
+      )}
     </h1>
   );
 }
